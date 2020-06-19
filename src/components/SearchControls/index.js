3172a20d26fa44a842792d3a3e1fe0e3418fa9ad
Chyna Davis <chyna.davis11@gmail.com>
2020-06-18 23:17:55 -0700

    add buttons & inputs; maybe a little too ambitious

--- src/components/SearchControls/index.js
+++ src/components/SearchControls/index.js
@@ -7,16 +7,19 @@ function SearchControls(props) {
             <div className="button-section">
                 <h4>sort employees by:</h4>
                 <div className="btn-group sort-btns" role="group" aria-label="Basic example">
+                    <button type="button" className="btn btn-outline-light btn-secondary btn-lg">first name</button>
                     <button type="button" className="btn btn-outline-light btn-secondary btn-lg">last name</button>
                     <button type="button" className="btn btn-outline-light btn-secondary btn-lg">date of birth</button>
+                    <button type="button" className="btn btn-outline-light btn-secondary btn-lg">username</button>
                     <button type="button" className="btn btn-outline-light btn-secondary btn-lg">location (state)</button>
                 </div>
             </div>
             <div className="button-section">
                 <h4>filter employees by:</h4>
                 <div className="input-group input-group-lg">
-                    <input type="text" class="form-control" placeholder="name"></input>
+                    <input type="text" class="form-control" placeholder="full name"></input>
                     <input type="text" class="form-control" placeholder="location (state)"></input>
+                    <input type="text" class="form-control" placeholder="username"></input>
                 </div>
             </div>
         </div>
